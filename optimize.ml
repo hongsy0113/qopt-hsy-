21ab47f126168038ca2c4560d3a432a6094d9360
hongsy0113 <87005840+hongsy0113@users.noreply.github.com>
2021-07-29 16:02:57 +0900

    Update optimize.ml

--- optimize.ml
+++ optimize.ml
@@ -4,5 +4,27 @@ open Vocab
 open Pp
 open Env
 
+module Hello : sig
+  val hello : unit -> unit
+ end = 
+ struct
+   let message = "Hello"
+   let hello () = print_endline message
+ end
+ 
+module Equiv =
+  struct
+  type t = (program, program) BatMap.t
+  let empty = BatMap.empty
+  let add = BatMap.add
+end
+(*
+let x_propagation : program -> program
+= fun smts-> 
+  let equiv = Equiv.empty in
+  let equiv = add []
+*)
+
 let optimize : program -> program 
-=fun stmts -> stmts (* TODO *)
+=fun stmts -> stmts(*[Include ("test"); Decl(QReg("q", 1)); Qop(Uop(Gate("h", [], ["q", Some 1])))]*)
+ (* TODO *)
